51a8bf91a7b111bad0f3296d3941d46a2f0235e6
FREDRAHUL0 <42122001+FREDRAHUL0@users.noreply.github.com>
2024-03-06 00:49:06 +0530

    Create output.tf

---
+++ output.tf
@@ -0,0 +1,10 @@
+output "show" {
+ value = aws_vpc.prod_vpc.id
+ }
+ output "show2" {
+    value = local.my_region
+   
+ }
+ output "my_output" {
+    value = "my id is $(aws_vpc.prod_vpc.id} and my arn is ${aws_vpc.prod_vpc.arn}"
+ } 
